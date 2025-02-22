73aa098463b497758200032bdd3bb9e0d36cc0f4
Ege Uysal <hello@egeuysal.com>
2025-02-22 04:17:22 +0000

    Style with tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Hello from "@/app/components/hello"
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import Hello from "@/app/components/hello";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   console.log("What am I, SERVER/CLIENT?");
 
   return (
-    <>
-      <h1 className="text-3xl">Welcome to Next.js!</h1>
-      <Hello></Hello>
-      <Analytics></Analytics>
-      <SpeedInsights></SpeedInsights>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-900">Welcome to Next.js!</h1>
+      <Hello />
+      <Analytics />
+      <SpeedInsights />
+    </div>
   );
 }
