3598a3d15f137edb1d4701dd75f1d0decf230b7b
Ege Uysal <hello@egeuysal.com>
2025-02-22 03:48:55 +0000

    Learn next.js

---
+++ app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use cleint";
+
+function Hello() {
+    console.log("I'm a client component!")
+
+    return (
+        <div>
+            <h1>Hello!</h1>
+        </div>
+    );
+}
+
+export default Hello;
