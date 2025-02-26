a59dbdbba9b6043e9f6552d3a8df819d5752ce4a
Ege Uysal <ahmetegeuysal@gmail.com>
2025-02-25 20:18:50 -0600

    Reset config

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['egeuysal.com'],
-  },
-  redirects: async () => [
-    {
-      source: '/old-page',
-      destination: '/new-page',
-      permanent: true,
-    },
-  ],
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false };
-    }
-    return config;
-  },
-  headers: async () => [
-    {
-      source: '/(.*)',
-      headers: [
-        { key: 'X-Frame-Options', value: 'DENY' },
-        { key: 'Content-Security-Policy', value: "default-src 'self'" },
-      ],
-    },
-  ],
-  experimental: { esmExternals: true },
+  /* config options here */
 };
 
 export default nextConfig;
